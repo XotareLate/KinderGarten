746ddc3126311713c133721363ca21d9fd19f862
Xotary <60504787+XotareLate@users.noreply.github.com>
2022-12-07 01:44:17 +0300

    Delete KinderGarten directory

--- KinderGarten/src/main/java/com/example/kindergarten/Configs.java
+++
@@ -1,9 +0,0 @@
-package com.example.kindergarten;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "Neket_2003";
-    protected String dbName = "groups";
-}
